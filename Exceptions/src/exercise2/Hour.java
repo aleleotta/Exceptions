f136bdda462bdd9b2589a1254aca06b8eacfdad5
Alessandro Leotta <Alessandro Leotta@HPaleleotta>
2023-05-15 09:21:49 +0200

    .

---
+++ Exceptions/src/exercise2/Hour.java
@@ -0,0 +1,76 @@
+package exercise2;
+import java.util.*;
+
+public class Hour {
+	private long hour = 0;
+	private long minute = 0;
+	private long second = 0;
+	
+	public Hour() {}
+	
+	public Hour(long hour, long minute, long second) {
+		this.hour = hour;
+		this.minute = minute;
+		this.second = second;
+	}
+	
+	public boolean secondsIncrement(long second) {
+		boolean confirm = true;
+		if(second > 0) {
+			this.second = this.second + second;
+			while(this.second >= 60) {
+				this.minute++;
+				this.second = this.second - 60;
+			}
+			while(this.minute >= 60) {
+				this.hour++;
+				this.minute = this.minute - 60;
+			}
+		} else {
+			confirm = false;
+		}
+		return confirm;
+	}
+	
+	@Override
+	public String toString() {
+		return "\t" + getHour() + ":" + getMinute() + ":" + getSecond() + "\n";
+	}
+	
+	public long getHour() {
+		return hour;
+	}
+
+	public void setHour(long hour) {
+		this.hour = hour;
+	}
+
+	public long getMinute() {
+		return minute;
+	}
+
+	public void setMinute(long minute) {
+		this.minute = minute;
+	}
+
+	public long getSecond() {
+		return second;
+	}
+
+	public void setSecond(long second) {
+		this.second = second;
+	}
+	
+}
+
+/*
+Crea tres excepciones nuevas de nombre NegativeSecondException, NegativeMinuteException y NegativeHourException.
+Las tres tienen que heredar de Exception.
+Partiendo de la clase Hora que diseñaste en el Tema 7, modifica los setter y los constructores para que:
+En caso de que se introduzca un segundo negativo se lance la excepción NegativeSecondException.
+En caso de que se introduzca un minuto negativo se lance la excepción NegativeMinuteException.
+En caso de que se introduzca una hora negativa se lance la excepción NegativeHourException.
+Crea una clase con un método main en donde se creen objetos de tipo Hora y se modifiquen sus atributos con los setters.
+Mete el código en un bloque try-catch que se encargue de capturar las excepciones creadas.
+Debe haber en este caso tres bloques catch, uno por excepción.
+*/
--- Exceptions/src/exercise2/Main.java
+++ Exceptions/src/exercise2/Main.java
@@ -3,7 +3,48 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
+		long hour = -1;
+		long minute = -1;
+		long second = -1;
 		Scanner sc = new Scanner(System.in);
+		while(hour < 0 || minute < 0 || second < 0) {
+			System.out.println("Introduce the following parameters.");
+			System.out.print("Hours: ");
+			hour = sc.nextLong();
+			System.out.print("Minutes: ");
+			minute = sc.nextLong();
+			System.out.print("Seconds: ");
+			second = sc.nextLong();
+			if(hour < 0 || minute < 0 || second < 0) {
+				System.out.println("\nMake sure all parameters are not negative!\n");
+			}
+		}
+		Hour time = new Hour(hour, minute, second);
+		int option = 0;
+		while(option != 3) {
+			System.out.print("\nOptions\n1) Print time\n2) Add seconds to time\n3) Exit program\n\nIntroduce an option: ");
+			option = sc.nextInt();
+			switch(option) {
+			case 1: //Print
+				System.out.print(time.toString());
+				break;
+			case 2: //Add seconds
+				long addedSeconds = 0;
+				System.out.print("\nIntroduce the seconds you want to add to the time: ");
+				addedSeconds = sc.nextLong();
+				if(time.secondsIncrement(addedSeconds) == false) {
+					System.out.println("Make sure the seconds are positive and above 0 otherwise no seconds will be added.");
+				}
+				System.out.print(time.toString());
+				break;
+			case 3:
+				break;
+			default: //Error
+				System.out.println("Introduce a valid option!");
+				break;
+			}
+		}
+		System.out.println("\nExiting program...\n\nProgram terminated.");
 		sc.close();
 	}
 }
